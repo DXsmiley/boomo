fe193f3cb78b8d760c4bb531d4b83a427415d55a
DXsmiley <dxsmiley@hotmail.com>
2016-10-02 20:56:45 +1100

    Layout of the game page

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var app = express();
 app.use('/static', express.static(__dirname + '/static/'));
 
 app.get('/', function(req, res) {
-    res.send(pug.renderFile(__dirname + '/templates/index.pug', {}));
+    res.send(pug.renderFile(__dirname + '/templates/index.pug'));
+});
+
+app.get('/game', function(req, res) {
+    res.send(pug.renderFile(__dirname + '/templates/game.pug'));
 });
 
 app.listen(3000, function() {
