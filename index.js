43cd995bb9bfc75d2deec442910baa108c0c6e04
DXsmiley <dxsmiley@hotmail.com>
2016-10-02 15:30:02 +1100

    Something that looks like a homepage

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var pug = require('pug');
+var app = express();
+
+app.use('/static', express.static(__dirname + '/static/'));
+
+app.get('/', function(req, res) {
+    res.send(pug.renderFile(__dirname + '/templates/index.pug', {}));
+});
+
+app.listen(3000, function() {
+    console.log('listening on *:3000');
+});
