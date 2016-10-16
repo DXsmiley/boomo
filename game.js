70ffe506750afa4cbb64703a56d6c87d0e61cda9
DXsmiley <dxsmiley@hotmail.com>
2016-10-16 19:09:38 +1100

    Implement play 2

--- game.js
+++ game.js
@@ -15,7 +15,7 @@ const CARD_NUMBERS = {
     'set30': 4,   // set the counter to 2
     'skip': 6,    // skip the next player
     'reverse': 4, // reverse the play order
-    'play2': 0    // next player players 2 cards
+    'play2': 8    // next player players 2 cards
 };
 
 const TOTAL_CARDS = Object.keys(CARD_NUMBERS).reduce((sum, key) => sum + CARD_NUMBERS[key]);
@@ -82,7 +82,7 @@ Game.prototype.nextPlayerInCycle = function() {
         if (next == -1) next = this.player_order.length - 1;
         if (next == this.player_order.length) next = 0;
     } while (this.alive_count > 1 && this.players[this.player_order[next]].lives == 0);
-    return this.players[this.player_order[next]];
+    return next;
 }
 
 // TODO: Change this to take the player ID, not the player object.
@@ -104,19 +104,19 @@ Game.prototype.playerDrawCards = function(player, number) {
 
 Game.prototype.advanceActivePlayer = function() {
     if (this.alive_count > 1) {
-        var next = this.active_index + this.play_direction;
-        if (next == -1) next = this.player_order.length - 1;
-        if (next == this.player_order.length) next = 0;
-        this.active_index = next;
-        var p = this.players[this.player_order[next]];
-        if (p.lives == 0) {
-            // XXX: This needs to be put back in when lives are properly implemented.
-            this.advanceActivePlayer();
+        var cur = this.players[this.player_order[this.active_index]];
+        if (cur.lives > 0 && cur.turns_to_take > 0) {
+            cur.turns_to_take -= 1;
         } else {
-            if (p.turns_to_take < 0) {
-                p.turns_to_take += 1;
-                this.advanceActivePlayer();
-            }
+            do {
+                this.active_index = this.nextPlayerInCycle();
+                cur = this.players[this.player_order[this.active_index]];
+                if (cur.lives > 0) {
+                    if (cur.turns_to_take < 0) {
+                        cur.turns_to_take += 1;
+                    } else break;
+                }
+            } while (true);
         }
     }
 }
@@ -149,7 +149,14 @@ Game.prototype.playCard = function(player, card) {
                 if (card == 'set0') this.bomb_timer = 0;
                 if (card == 'set30') this.bomb_timer = 30;
                 if (card == 'set60') this.bomb_timer = 60;
-                if (card == 'skip') this.nextPlayerInCycle().turns_to_take -= 1;
+                if (card == 'skip') {
+                    var next = this.nextPlayerInCycle();
+                    this.players[this.player_order[next]].turns_to_take -= 1;
+                }
+                if (card == 'play2') {
+                    var next = this.nextPlayerInCycle();
+                    this.players[this.player_order[next]].turns_to_take += 1;
+                }
                 if (card == 'draw1') this.allDraw(1, player);
                 if (card == 'draw2') this.allDraw(2, player);
                 if (card == 'reverse') this.play_direction *= -1;
